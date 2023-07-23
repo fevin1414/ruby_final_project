737c2313c1d8ec866048460ff06fb0a1fa7c0e29
Fevin Biju <fevinbiju1414@gmail.com>
2023-07-23 01:06:47 -0500

    added product display and show

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -2,33 +2,29 @@ ActiveAdmin.register Product do
   permit_params :name, :description, :price, :stock, :category_id, :on_sale,
                 :newly_added, :recently_updated, product_images_attributes: [:id, :image, :_destroy]
 
-                controller do
-                  def update
-                    # Ensure the permitted attributes are updated in the model
-                    super do |format|
-                      redirect_to collection_path and return if resource.valid?
-                    end
-                  end
-                end
-
-                form do |f|
-                  f.inputs 'Product Details' do
-                    f.input :name
-                    f.input :description
-                    f.input :price
-                    f.input :stock
-                    f.input :category
-                    f.input :on_sale, label: 'On Sale'
-                    f.input :newly_added, label: 'Newly Added'
-                    f.input :recently_updated, label: 'Recently Updated'
-                    f.has_many :product_images, allow_destroy: true do |image|
-                      image.input :image, as: :file, input_html: { accept: 'image/*' }
-                    end
-                  end
-                  f.actions
-                end
+  form do |f|
+    f.inputs 'Product Details' do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :stock
+      f.input :category
+      f.input :on_sale, label: 'On Sale'
+      f.input :newly_added, label: 'Newly Added'
+      f.input :recently_updated, label: 'Recently Updated'
+      f.has_many :product_images, allow_destroy: true do |image|
+        image.input :image, as: :file, input_html: { accept: 'image/*' }
+      end
+    end
+    f.actions
+  end
 
   show do
+    h1 product.name
+    p "Description: #{product.description}"
+    p "Price: #{product.price}"
+    p "Available Stock: #{product.stock}"
+
     attributes_table do
       row :name
       row :description
@@ -42,21 +38,24 @@ ActiveAdmin.register Product do
       row :updated_at
     end
 
-    panel 'Images' do
-      table_for product.product_images do
-        column :thumbnail do |img|
+    panel 'Main Image' do
+      # Display the thumbnail as the main image
+      if product.product_images.any?
+        image_tag product.product_images.first.image.url(:thumbnail)
+      else
+        # Replace 'placeholder_image_url' with the placeholder image URL if no images are present
+        image_tag 'https://lorempixel.com/200/200/', class: 'placeholder-image'
+      end
+    end
+
+    panel 'Product Images' do
+      table_for product.product_images.drop(1) do
+        column :image do |img|
           image_tag img.image.url(:thumbnail)
         end
-        column :left_image do |img|
-          image_tag img.image.url(:left_image)
-        end
-        column :back_image do |img|
-          image_tag img.image.url(:back_image)
-        end
-        column :side_image do |img|
-          image_tag img.image.url(:side_image)
-        end
       end
+      # Display a placeholder image for the product images section if no additional images are present
+      image_tag 'https://lorempixel.com/200/200/', class: 'placeholder-image' unless product.product_images.drop(1).any?
     end
 
     panel 'Reviews' do
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  resources :products, only: [:index, :show]
+
   get 'about_pages/show'
   get 'contact_pages/show'
+
   # Devise routes for regular users
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
 
@@ -8,23 +12,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  # Route for the admin dashboard
-  authenticated :user, ->(u) { u.admin? } do
-    root to: 'admin#index', as: :custom_admin_dashboard
-    # Other routes for the custom-made index pages for admin can be added here.
-  end
-
-  # Route for the user dashboard
-  authenticated :user, ->(u) { !u.admin? } do
-    root to: 'users#index', as: :custom_user_dashboard
-    # Other routes for the custom-made index pages for regular users can be added here.
-  end
-
-  unauthenticated do
-    root to: 'devise/sessions#new'
-  end
-
   # Other routes for your application can be added here.
   get '/contact', to: 'contact_pages#show'
-get '/about', to: 'about_pages#show'
+  get '/about', to: 'about_pages#show'
 end
