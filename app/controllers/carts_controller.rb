fa5c30f0322a62c3707662bd810f6f796843dc8a
Fevin Biju <fevinbiju1414@gmail.com>
2023-07-26 01:17:29 -0500

    added hash messages

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,23 +7,25 @@ class CartsController < ApplicationController
 
   def add_item
     @cart.add_item(params[:product_id], params[:quantity])
+    flash[:notice] = 'Item added to cart successfully!'
     redirect_to cart_path
   end
 
   def remove_item
     @cart.remove_item(params[:product_id])
+    flash[:notice] = 'Item removed from cart successfully!'
     redirect_to cart_path
   end
 
   def update_item
     @cart.update_item(params[:product_id], params[:quantity])
+    flash[:notice] = 'Cart updated successfully!'
     redirect_to cart_path
   end
 
   def total
     @items.sum { |product_id, quantity| Product.find(product_id).price * quantity.to_d }
   end
-
 
   private
 
